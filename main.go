815cc0d952ef3674c739d17a306b676380a250aa
tharakeshrajesh <tharakeshrajesh3@gmail.com>
2026-05-07 17:34:27 -0500

    Optimized code and made error handling a little better

--- main.go
+++ main.go
@@ -3,11 +3,19 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"strings"
 	"net/http"
 	"net/smtp"
 	"os"
 	"github.com/joho/godotenv"
 )
+
+type EmailRequest struct {
+    Sender    string `json:"sender"`
+    Recipient string `json:"recipient"`
+    Subject   string `json:"subject"`
+    Body      string `json:"body"`
+}
 
 func writeJSON(w http.ResponseWriter, status int, data any) {
 	w.Header().Set("Content-Type", "application/json")
@@ -16,10 +24,18 @@ func writeJSON(w http.ResponseWriter, status int, data any) {
 }
 
 func writeError(w http.ResponseWriter, status int, message string) {
-	writeJSON(w, status, map[string]string{"error": message})
+	errorcode := message[:3]
+
+	switch message {
+	case "553":
+		message = "An invalid email address was given."
+	//case ""
+	}
+
+	writeJSON(w, status, map[string]string{"error": message, "errorcode": errorcode})
 }
 
-func sendEmail(name string, to string, subject string, body string) error {
+func sendEmail(req EmailRequest) error {
 	_ = godotenv.Load()
 
 	pass := os.Getenv("SMTP_PASS")
@@ -32,20 +48,20 @@ func sendEmail(name string, to string, subject string, body string) error {
 		return fmt.Errorf("SMTP_EMAIL not set")
 	}
 	
-	fromHeader := `"` + name + `"` + " <no-reply@3272010.xyz>"
+	fromHeader := `"` + req.Sender + `"` + " <no-reply@3272010.xyz>"
 	host := "smtp.gmail.com"
 	addr := host + ":587"
 
 	auth := smtp.PlainAuth("", email, pass, host)
 
 	msg := []byte("From: " + fromHeader + "\r\n" +
-		"To: " + to + "\r\n" +
-		"Subject: " + subject + "\r\n" +
+		"To: " + req.Recipient + "\r\n" +
+		"Subject: " + req.Subject + "\r\n" +
 		"Content-Type: text/plain; charset=\"utf-8\"\r\n" +
 		"\r\n" +
-		body + "\r\n")
+		req.Body + "\r\n")
 
-	if err := smtp.SendMail(addr, auth, email, []string{to}, msg); err != nil {
+	if err := smtp.SendMail(addr, auth, email, []string{req.Recipient}, msg); err != nil {
 		return err
 	}
 	return nil
@@ -56,18 +72,43 @@ func main() {
 
 	mux := http.NewServeMux()
 
-	mux.HandleFunc("GET /sendEmail/", func(w http.ResponseWriter, r *http.Request) {
-		name := r.URL.Query().Get("name")
-		to := r.URL.Query().Get("recipient")
-		subject := r.URL.Query().Get("subject")
-		body := r.URL.Query().Get("body")
+	mux.HandleFunc("/sendEmail/", func(w http.ResponseWriter, r *http.Request) {
 
-		result := sendEmail(name, to, subject, body)
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+
+		if r.Method == http.MethodOptions {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+
+		if r.Method != http.MethodPost {
+			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+			return
+		}
+		
+		var req EmailRequest
+
+		err := json.NewDecoder(r.Body).Decode(&req)
+		if err != nil {
+			writeError(w, 500, "There was an error in parsing the JSON")
+			return
+		}
+
+		if strings.ReplaceAll(req.Sender, " ", "") == "" {
+			req.Sender = "Authentication by 3272010.xyz"
+		}
+		if strings.ReplaceAll(req.Subject, " ", "") == "" {
+			req.Subject = "Authentication by 3272010.xyz"
+		}
+
+		result := sendEmail(req)
 
 		if result == nil {
 			writeJSON(w, 200, "success")
 		} else {
-			writeError(w, 500, "idk")
+			writeError(w, 500, result.Error())
 		}
 		
 	})
